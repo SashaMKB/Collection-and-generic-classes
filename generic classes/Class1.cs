82ac9ee80b3a046b71a65fd1a50e8504f33305c1
Alexander <73365442+SashaMKB@users.noreply.github.com>
2022-03-29 16:54:00 +0400

    Add files via upload

---
+++ generic classes/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Second_Task
+{
+    class LocalFileLogger<T>:ILogger
+    {
+        public T Value { get; set; }
+        private string WayToFile = "C:\\Users\\79196\\Desktop\\Homework\\Second_Task\\New_File";
+
+        public LocalFileLogger(string WayToFile) { }
+
+        public void  LogInfo(string message)
+        {
+            File.AppendAllTextAsync(WayToFile, $"[Info]: [{Value}] : {message}");
+        }
+        public void LogWarning(string message)
+        {
+            File.AppendAllTextAsync(WayToFile, $"[Warning] : [{Value}] : {message}");
+        }
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllTextAsync(WayToFile, $"[Error] : [{Value}] : {message}. {ex.Message}");
+        }
+    }
+}
--- generic classes/Program.cs
+++ generic classes/Program.cs
@@ -1,1 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Second_Task;
 
+namespace Second_Task
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string WayToFile = "C:\\Users\\79196\\Desktop\\Homework\\Second_Task\\New_File";
+            var genericClassIntenger = new LocalFileLogger<int>(WayToFile);
+            genericClassIntenger.Value = 100;
+            genericClassIntenger.LogInfo("intenger create ");
+            genericClassIntenger.LogWarning("warning! ");
+            genericClassIntenger.LogError("Error! ", new FormatException());
+
+
+            var genericClassString = new LocalFileLogger<string>(WayToFile);
+            genericClassString.Value = "some_string";
+            genericClassString.LogInfo("string create");
+            genericClassString.LogWarning("Warning !");
+            genericClassString.LogError("Error!", new FormatException());
+        }
+    }
+}
